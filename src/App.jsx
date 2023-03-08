6bf07821f4dc2f8dbef4189730e9efb204e194e2
Allan D <afdeltour@gmail.com>
2023-03-08 19:26:04 +0100

    ADD Banner

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Row from './Components/Row/Row'
 import requests from './Features/requests'
 import './App.css'
+import Banner from './Components/Banner/Banner'
+import NavBar from './Components/Navbar/NavBar'
 
 export function App() {
   return (
-    <div className="App">
-      {/* Nav à créer */}
-      {/* Banner à créer */}
+    <div className="app">
+      <NavBar />
+      <Banner />
       <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
